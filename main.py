d22787bbb1cf69704fa5e709435ee8bd5b28bcbb
petuxonchik <rozetka33376@gmail.com>
2024-09-24 21:15:34 +0300

    3

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-number_one=float(input("Введите первое число для проверки , какое из двух окажется меньше: "))
-number_two=float(input("Введите втрое число для проверки , какое из двух окажется меньше: "))
-if number_one > number_two:
-    print(f"Второе число: {number_two} меньше первого")
-elif number_two > number_one:
-    print(f"Первое число: {number_one} меньше второго")
-else:
-     print(f"Первое число {number_one} равно Второму числу {number_two}")
+#запрос данных с клавиатуры
 
+day = int(input("Введите день (число): "))
+month = int(input("Введите месяц (число): "))
+
+# Проверяем, к какому времени года относится введенная дата
+if (month == 3 and day >= 1) or (month in [4, 5]) or (month == 6 and day < 1):
+    season = "Весна"
+elif (month == 6 and day >= 1) or (month in [7, 8]) or (month == 9 and day < 1):
+    season = "Лето"
+elif (month == 9 and day >= 1) or (month in [10, 11]) or (month == 12 and day < 1):
+    season = "Осень"
+else:
+    season = "Зима"
+
+# Выводим результат
+print(f"Дата {day}.{month} относится к сезону: {season}")
